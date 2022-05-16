fc10eb2f710c07be9d58d2f7daf8192c66c9009a
handriaparfait <handria.parfait@gmail.com>
2022-05-16 19:50:38 -0400

    CRUD mis en place

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,21 +2,112 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
 use App\Repository\ArticleRepository;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+
 class ArticleController extends AbstractController
 {
+    private $em;
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+
+
     #[Route('/', name: 'app_article')]
     public function index(ArticleRepository $articlerepo): Response
     {
 
-        $article = $articlerepo->findAll();
+        $article = $articlerepo->findBy([], ['createdAt' => 'DESC']);
 
         return $this->render('article/index.html.twig', [
             'article' => $article
         ]);
     }
+
+    #[Route('/article/{id<[0-9]+>}', name: 'app_show_article'/*, methods: 'GET|POST'*/)]
+    public function detailArticle(Article $article)
+    {
+        return $this->render('article/detail.html.twig', ['article' => $article]);
+    }
+
+    #[Route('/create', name: 'app_create', methods: ["POST", "GET"])]
+    public function create(Request $request): Response
+    {
+        
+
+        $form = $this->createFormBuilder(new Article)
+            ->add('Nom_Produit', TextType::class)
+            ->add('Description', TextareaType::class)
+            ->getForm()
+        ;
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            $this->em->persist($form->getData());
+            $this->em->flush();
+
+            return $this->redirectToRoute('app_article');
+            
+        }
+
+        return $this->render('article/create.html.twig', [
+            'formulaire' => $form->createView()
+        ]);
+    }
+
+    #[Route('/article/{id<[0-9]+>}/edit', name: 'app_article_edit', methods: 'GET|POST')]
+    public function editer(Article $article, Request $request): Response
+    {
+
+        $form = $this->createFormBuilder($article)
+            ->add('Nom_Produit', TextType::class)
+            ->add('Description', TextareaType::class)
+            ->getForm()
+        ;
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            $this->em->flush();
+
+            return $this->redirectToRoute('app_article');
+            
+        }
+
+
+        return $this->render('article/edit.html.twig', [
+            'article' => $article,
+            'formulaire' => $form->createView(),
+        ]);
+
+    }
+
+    #[Route('/article/{id<[0-9]+>}/delete', name: 'app_article_delete', methods: ['GET'])]
+    public function delete(Request $request, Article $article)
+    {     
+
+        $this->em->remove($article);
+        $this->em->flush();
+
+             
+        return $this->redirectToRoute('app_article');
+
+    }
+
+
+
+
 }
--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -3,9 +3,11 @@
 namespace App\Entity;
 
 use App\Repository\ArticleRepository;
+use DateTimeImmutable;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: ArticleRepository::class)]
+#[ORM\HasLifecycleCallbacks]
 class Article
 {
     #[ORM\Id]
@@ -18,6 +20,12 @@ class Article
 
     #[ORM\Column(type: 'text')]
     private $Description;
+
+    #[ORM\Column(type: 'datetime_immutable', options:["default" => "CURRENT_TIMESTAMP"])]
+    private $createdAt;
+
+    #[ORM\Column(type: 'datetime_immutable', options:["default" => "CURRENT_TIMESTAMP"])]
+    private $updateAt;
 
     public function getId(): ?int
     {
@@ -47,4 +55,41 @@ class Article
 
         return $this;
     }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getUpdateAt(): ?\DateTimeImmutable
+    {
+        return $this->updateAt;
+    }
+
+    public function setUpdateAt(\DateTimeImmutable $updateAt): self
+    {
+        $this->updateAt = $updateAt;
+
+        return $this;
+    }
+
+    #[ORM\PrePersist]
+    #[ORM\PreUpdate]
+
+    public function updateTimestamp()
+    {
+        if ($this->getCreatedAt() === null){
+            $this->setCreatedAt(new \DateTimeImmutable);
+        }
+        $this->setUpdateAt(new \DateTimeImmutable);
+    }
+
+   
 }
